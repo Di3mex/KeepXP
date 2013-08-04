ecfe7bbe2c55f63e5d4540cdaeb83cd6a9f5154c
max <di.em.ex@gmx.de>
2013-08-04 09:28:13 +0200

    We are GNU Affero

--- src/test/de/diemex/keepxp/ExpBottleTest.java
+++ src/test/de/diemex/keepxp/ExpBottleTest.java
@@ -1,3 +1,23 @@
+/*
+ * This file is part of
+ * KeepXP Server Plugin for Minecraft
+ *
+ * Copyright (C) 2013 Diemex
+ *
+ * KeepXP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KeepXP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero Public License
+ * along with KeepXP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.diemex.keepxp;
 
 
